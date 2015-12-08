d6853285634cea0779d8ecaeb10979f21ab47602
rob-datarati <rob.thwaites@datarati.com.au>
2015-12-09 06:46:33 +1100

    Busted dustcloud generator to post to Codepen - only .sets random on first element of class '.dust'

--- index.php
+++ index.php
@@ -47,13 +47,14 @@ var selectAll = function(s) {
 
 var duplicate = function(e, eNumber) {
     var element = select(e);
-//    console.log(element);
+    var className = element.getAttribute('class');
     for (var i=0; i < eNumber; i++){
         var newElement = element.cloneNode(true);
-//        var theName= newElement.getAttribute('class') + ' ' + (newElement.getAttribute('class') + (i + 1));
-//        newElement.setAttribute('class', theName);
-        element.parentNode.insertBefore(newElement, element.nextSibling);
-    }
+        var newClass = className + (i+1);
+            newElement.setAttribute('class', className + ' ' + newClass);
+            element.parentNode.insertBefore(newElement, element.nextSibling);
+    };
+    element.setAttribute('class', className + ' ' + className + '0');
 };
 
 var hey = select('.hey'),
@@ -62,7 +63,6 @@ overhere = select('.overhere'),
 board = select('.board'),
 squareleft = select('.squareleft'),
 squareright= select('.squareright'),
-//dust = selectAll('.dust'),
 youmadeit = select('.youmadeit'),
 underline = select('.underline'),
 lights = select('.lights'),
@@ -77,19 +77,16 @@ tl.set([hey, you, overhere, board, underline, thanks, glint, lights, youmadeit],
 
 var clouds = function(cloud, numClouds){
     duplicate(cloud, numClouds);
-    var allClouds = selectAll(cloud);
-//    console.log(allClouds);
     for (var i=0; i < numClouds+1; i++){
-        var thisCloud = allClouds[i];
-        console.log(thisCloud);
-//        tl
-//        .set(thisCloud, {y:randMinMax(100, 200), opacity: 0})
-//        .to(thisCloud, randMinMax(1,2), {y:randMinMax(-150, -200)}, 'startDust')
-//        .to(thisCloud, randMinMax(1,2), {opacity: 1, repeat:1, yoyo: true}, 'startDust');
+        var thisCloud = select(cloud + i);
+        tl
+        .set(thisCloud, {x:randMinMax(200, 300), y:randMinMax(0, 100),  opacity: 0})
+        .to(thisCloud, 1, {y:randMinMax(-150, -200), x:randMinMax(-200, 100), opacity: randMinMax(0.3, 1), repeat:5, yoyo: true}, 'startDust')
+//        .to(thisCloud, 1, {opacity: 1, repeat:1, yoyo: true}, 'startDust');
     }
 }
 
-//clouds('.dust', 4);
+clouds('.dust', 4);
 
     // SET UP
 
@@ -132,74 +129,6 @@ var clouds = function(cloud, numClouds){
 //        ease: Power0.easeIn
 //    }, 'dustup')
 //    ;
-
-
-
-    /*
-GSAP JS Demo
-http://www.greensock.com/gsap-js/
-Animation and Bezier motion:
-http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js
-*/
-
-// DUPLICATE ELEMENT
-
-var duplicate = function(e, eNumber) {
-    var element = select(e);
-//    console.log(element);
-    for (var i=0; i < eNumber; i++){
-        var newElement = element.cloneNode(true);
-//        var theName= newElement.getAttribute('class') + ' ' + (newElement.getAttribute('class') + (i + 1));
-//        newElement.setAttribute('class', theName);
-        element.parentNode.insertBefore(newElement, element.nextSibling);
-    }
-};
-
-var tl2;
-
-
-function addCloud(element, cloudNumber) {
-    var target = select(element);
-    for (var i=0; i < cloudNumber; i++){
-        var newElement = target.cloneNode(true);
-        target.parentNode.insertBefore(newElement, target.nextSibling);
-        var random = randMinMax(200,400);
-        TweenMax.set(element, {
-            y: 0,
-            x: random,
-            opacity: 0
-        })
-        TweenMax.to(element, 0.1,{
-            opacity: 1,
-            y: 1,
-            x: 1
-        })
-        TweenMax.to(element, 5, {
-            y: randMinMax(200,400),
-            delay: randMinMax(1,5)
-        })
-        TweenMax.to(element, 5, {
-            opacity: 1,
-            repeat: 1,
-            yoyo: true
-        })
-    }
-}
-
-addCloud('.dust', 5);
-
-
-
-function buildTimeline() {
-  tl2 = new TimelineMax();
-  for (i = 0 ; i< 20; i++){
-    tl2.add(addCloud(), i * 0.17);
-  }
-}
-
-//buildTimeline();
-
-
 
 
         </script>
