413081a16647ce8f99271d08b7fc75a71b762fb7
rob-datarati <rob.thwaites@datarati.com.au>
2016-01-11 15:58:21 +1100

    Cloud emitter function built

--- js/balloons.js
+++ js/balloons.js
@@ -25,7 +25,8 @@ var
     You = select('#blYou'),
     Made = select('#blMade'),
     It = select('#blIt'),
-    cloud = select('.cloud')
+    cloud = selectAll('.cloud'),
+    cloudGroup = selectAll('.cloudGroup')
     ;
 
 
@@ -184,6 +185,7 @@ var aMade = new TimelineMax();
 var aIt = new TimelineMax();
 
 aYou
+.call(toggleClouds)
 .set([You, Made], {
     y: 800,
     x: -100,
@@ -281,6 +283,7 @@ aIt
     scaleY: 0.7,
     ease: Back.easeInOut
 })
+.call(toggleClouds)
 .call(function(){
     blYou.kill();
     blMade.kill();
@@ -306,15 +309,11 @@ aBalloons.add(aYou, 0).add(aMade, 1.5).add(aIt, 4);
 testTL.add(aString);
 //testTL.add(aCamera, 0);
 
-var dupElement = function(element) {
-  var newElement = element.cloneNode(true);
-  element.parentNode.insertBefore(newElement, element.nextSibling);
-  return newElement;
-};
-
-
 
 // CLOUDS ANIMATION
+
+TweenMax.set(cloudGroup[0], {rotation: 45, y:-400, x: 600});
+TweenMax.set(cloudGroup[1], {rotation: 45, y:-400, x: 600, scale: 2, opacity: 0.5});
 
 var tapOn = false,
   cloudsTL = new TimelineMax();
@@ -322,28 +321,24 @@ var tapOn = false,
 var now = performance.now.bind(performance);
 var ease = Power0.easeOut;
 var cache = [];
-var emitter = select(".emitter");
 var last = now();
-var frequency = 200;
+var numClouds = cloud.length;
+var frequency = 500;
+//console.log(numClouds);
 
-
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < numClouds; i++) {
   createParticle();
 }
 
 function createParticle() {
 
-  var particle = dupElement(cloud);
-
-  var x = randMinMax(0, -800);
-  var color = randMinMax(0, 200);
-  var time1 = randMinMax(1, 2);
-  var time2 = randMinMax(1, 2);
+//  var x = [-800, -600, -200, -100, 0, 100, 200, 300];
+var particle = cloud[i];
 
   var tl = new TimelineLite({ paused: true, onComplete: onComplete })
-    .set(particle, { autoAlpha: 0.7, background:"hsl(" + color + ", 90%, 60%)"})
-    .to(particle, time1, { x: x, y: 800, ease: ease })
-    .to({}, time2, {}); // Just a delay so it will sit at the bottom before restarting
+  .set(particle, { autoAlpha: 1 })
+    .to(particle, 1, { /*x: x[i],*/ y: 1300, ease: ease, scale: 2 })
+    .to({}, 2, {}); // Just a delay so it will sit at the bottom before restarting
 
   function onComplete() {
     TweenMax.set(particle, { autoAlpha: 0 });
@@ -355,7 +350,7 @@ function createParticle() {
 }
 
 
-$('svg').click(function() {
+function toggleClouds() {
   tapOn = !tapOn;
   if (tapOn) {
 
@@ -365,7 +360,7 @@ $('svg').click(function() {
 
     TweenMax.ticker.removeEventListener("tick", emit);
   }
-});
+};
 
 function emit() {
 
@@ -379,5 +374,5 @@ function emit() {
     tween && tween.play(0);
     last = current;
   }
-}
+};
 
